19e34d914d56529cfddd4193fa5b48d1749be549
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-21 09:51:45 +0100

    Doc

--- R/create_output_file.R
+++ R/create_output_file.R
@@ -1,4 +1,7 @@
 #' Process an input file
+#' @param input_filename name of the input file,
+#'   as created by \code{\link{create_input_files}}
+#' @param output_filename name of the output file
 #' @author Richel J.C. Bilderbeek
 #' @export
 create_output_file <- function(
