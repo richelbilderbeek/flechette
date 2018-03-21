de7678e59c41d644fb377f89c5602516005ea61f
richelbilderbeek <richel@richelbilderbeek.nl>
2018-03-21 09:41:53 +0100

    Output file creation works

--- tests/testthat/test-create_output_file.R
+++ tests/testthat/test-create_output_file.R
@@ -15,6 +15,15 @@ test_that("use", {
     output_filename = output_filename
   )
   testthat::expect_true(file.exists(output_filename))
+
+  out <- readRDS(output_filename)
+  testthat::expect_true("parameters" %in% names(out))
+  testthat::expect_true("incipient_tree" %in% names(out))
+  testthat::expect_true("species_tree" %in% names(out))
+  testthat::expect_true("alignment" %in% names(out))
+  testthat::expect_true("trees" %in% names(out))
+  testthat::expect_true("estimates" %in% names(out))
+
   file.remove(input_filenames)
   file.remove(output_filename)
 })
