81fccaf84dbe3bd9e4ed7fac15be51aacd98bfb7
richelbilderbeek <richel@richelbilderbeek.nl>
2018-04-10 22:15:52 +0200

    EOD

--- R/create_input_files.R
+++ R/create_input_files.R
@@ -15,39 +15,28 @@ create_input_files <- function(
     for (speciation_completion_rate in c(0.1, 0.3, 1.0, 1000000)) {
       for (extinction_rate in c(0.0, 0.1, 0.2)) {
         if (extinction_rate > speciation_initiation_rate) next
-        for (sampling_method in c("youngest", "oldest")) {
-          for (mutation_rate in c(0.05)) {
-            for (sequence_length in c(1000, 10000)) {
-              for (alignment_rng_seed in seq(1, 2)) {
-                for (beast2_rnd_seed in seq(1, 2)) {
-                  # Always use different trees
-                  for (tree_sim_rng_seed in seq(index, index + 10)) {
+        sampling_method <- "youngest"
+        if (i %% 2 == 1) sampling_method <- "oldest"
 
-                    filename <- paste0(index, ".RDa")
-                    params <- create_params(
-                      speciation_initiation_rate = speciation_initiation_rate,
-                      speciation_completion_rate = speciation_completion_rate,
-                      extinction_rate = extinction_rate,
-                      crown_age = 15,
-                      crown_age_sigma = 0.01,
-                      sampling_method = sampling_method,
-                      mutation_rate = mutation_rate,
-                      sequence_length = sequence_length,
-                      mcmc_length = mcmc_length,
-                      minimal_ess = minimal_ess,
-                      tree_sim_rng_seed = tree_sim_rng_seed,
-                      alignment_rng_seed = alignment_rng_seed,
-                      beast2_rnd_seed = beast2_rnd_seed
-                    )
-                    saveRDS(object = params, file = filename)
-                    index <- index + 1
-                    filenames <- c(filenames, filename)
-                  }
-                }
-              }
-            }
-          }
-        }
+        filename <- paste0(index, ".RDa")
+        params <- create_params(
+          speciation_initiation_rate = speciation_initiation_rate,
+          speciation_completion_rate = speciation_completion_rate,
+          extinction_rate = extinction_rate,
+          crown_age = 15,
+          crown_age_sigma = 0.01,
+          sampling_method = sampling_method,
+          mutation_rate = 1000 / 15,
+          sequence_length = 15000,
+          mcmc_length = mcmc_length,
+          minimal_ess = minimal_ess,
+          tree_sim_rng_seed = index,
+          alignment_rng_seed = index,
+          beast2_rnd_seed = index
+        )
+        saveRDS(object = params, file = filename)
+        index <- index + 1
+        filenames <- c(filenames, filename)
       }
     }
   }
