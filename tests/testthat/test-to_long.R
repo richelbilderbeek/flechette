54150508c6cfa1d7c60db9472ee77aa54b34a8a7
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-01 10:49:19 +0200

    Fix test

--- tests/testthat/test-to_long.R
+++ tests/testthat/test-to_long.R
@@ -5,9 +5,11 @@ test_that("use", {
   n_files <- 3
   n_nltts <- 3 # Called X, X.1 and X.2
   df <- data.frame(
-    speciation_initiation_rate = stats::runif(n = n_files),
-    speciation_completion_rate = stats::runif(n = n_files),
-    extinction_rate = stats::runif(n = n_files),
+    sirg = stats::runif(n = n_files),
+    siri = stats::runif(n = n_files),
+    scr = stats::runif(n = n_files),
+    erg = stats::runif(n = n_files),
+    eri = stats::runif(n = n_files),
     crown_age = rep(15, n_files),
     crown_age_sigma = rep(0.01, n_files),
     sampling_method = c("youngest", "oldest", "random"),
