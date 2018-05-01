ef5544db5e5196978668bf9ef963ff498ef81e4c
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-01 10:19:37 +0200

    @lintr-bot

--- tests/testthat/test-pbd_expected_n_extant.R
+++ tests/testthat/test-pbd_expected_n_extant.R
@@ -4,11 +4,11 @@ test_that("minimal use", {
   
   testthat::expect_silent(
     pbd_expected_n_extant(
-      crown_age = 1.0, 
-      scr = 1.0, 
-      sirg = 1.0, 
-      siri = 1.0, 
-      erg = 0.0, 
+      crown_age = 1.0,
+      scr = 1.0,
+      sirg = 1.0,
+      siri = 1.0,
+      erg = 0.0,
       eri = 0.0
     )
   )
@@ -16,27 +16,27 @@ test_that("minimal use", {
 
 test_that("bigger crown age results in more lineages", {
 
-  scr <- 1.0 
-  sirg <- 1.0 
-  siri <- 1.0 
-  erg <- 0.0 
+  scr <- 1.0
+  sirg <- 1.0
+  siri <- 1.0
+  erg <- 0.0
   eri <- 0.0
 
   n_lineages_young <- pbd_expected_n_extant(
-    crown_age = 1.0, 
-    scr = scr, 
-    sirg = sirg, 
-    siri = siri, 
-    erg = erg, 
+    crown_age = 1.0,
+    scr = scr,
+    sirg = sirg,
+    siri = siri,
+    erg = erg,
     eri = eri
   )
 
   n_lineages_old <- pbd_expected_n_extant(
-    crown_age = 2.0, 
-    scr = scr, 
-    sirg = sirg, 
-    siri = siri, 
-    erg = erg, 
+    crown_age = 2.0,
+    scr = scr,
+    sirg = sirg,
+    siri = siri,
+    erg = erg,
     eri = eri
   )
   testthat::expect_lt(n_lineages_young,n_lineages_old)
@@ -65,11 +65,11 @@ test_that("abuse", {
 
   testthat::expect_error(
     pbd_expected_n_extant(
-      crown_age = -123456, # Error 
-      scr = 1.0, 
-      sirg = 1.0, 
-      siri = 1.0, 
-      erg = 0.0, 
+      crown_age = -123456, # Error
+      scr = 1.0,
+      sirg = 1.0,
+      siri = 1.0,
+      erg = 0.0,
       eri = 0.0
     ),
     "'crown age' must be non-zero and positive"
