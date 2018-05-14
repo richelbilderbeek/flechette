e34a062b84bfec495292065fa6e40f89cf4bb07f
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-14 19:35:26 +0200

    Grunt work for @J-Damhuis and @Tomdkkr :-)

---
+++ R/pbd_sim_checked.R
@@ -0,0 +1,49 @@
+#' Checked and safer version of PBD::pbd_sim
+#' @param erg extinction rate of a good species
+#' @param eri extinction rate of an incipient species
+#' @param scr speciation completion rate
+#' @param sirg speciation initiation rate of a good species
+#' @param siri speciation initiation rate of an incipient species
+#' @param stem_age stem age. Set either the stem age or the crown age.
+#' @param crown_age crown age. Set either the stem age or the crown age.
+#' @param max_n_taxa maximum number of taxa. If this value is exceeded, 
+#'   the simulation is aborted and removed.
+pbd_sim_checked <- function(
+  erg,
+  eri,
+  scr,
+  sirg,
+  siri,
+  stem_age = NULL,
+  crown_age = NULL,
+  max_n_taxa = Inf  
+) {
+  # TODO: use if and stop
+  testit::assert(erg >= 0.0)
+  testit::assert(eri >= 0.0)
+  testit::assert(sirg >= 0.0)
+  testit::assert(siri >= 0.0)
+  testit::assert(scr >= 0.0)
+  testit::assert(xor(is.null(crown_age), is.null(stem_age)))
+  testit::assert(max_n_taxa >= 0.0)
+  
+  # age
+  age <- stem_age
+  if (is.null(age)) age <- crown_age
+  testit::assert(!is.null(age))
+  testit::assert(age > 0.0)
+  
+  # soc
+  soc <- 1
+  if (!is.null(crown_age)) soc <- 2
+  testit::assert(!is.null(soc))
+  testit::assert(soc == 1 || soc == 2)
+
+  PBD::pbd_sim(
+    pars = c(sirg, scr, siri, erg, eri),
+    age = age,
+    soc = soc,
+    plotit = FALSE,
+    limitsize = max_n_taxa
+  )
+}
---
+++ tests/testthat/test-pbd_sim_checked.R
@@ -0,0 +1,111 @@
+context("pbd_sim_checked")
+
+test_that("use", {
+  
+  skip("TODO for @J-Damhuis and @Tomdkkr")
+  
+  expect_silent(
+    pbd_sim_checked(
+      ... # TODO
+    )
+  )
+})
+
+test_that("PBD::pbd_sim and pbd_sim_checked must give same results", {
+  
+  skip("TODO for @J-Damhuis and @Tomdkkr")
+  
+  # This will be harder!
+  # You will have to duck to compare elements of lists
+  
+  expect_equal(
+    pbd_sim_checked(
+      ... # TODO
+    ),
+    PBD::pbd_sim(
+      ... # TODO
+    )
+  )
+})
+
+test_that("abuse", {
+
+  skip("TODO for @J-Damhuis and @Tomdkkr")
+  
+  expect_error(
+    pbd_sim_checked(
+      ..., # TODO
+      erg = -123
+    ), 
+    "'erg' must be positive"
+  )
+
+  expect_error(
+    pbd_sim_checked(
+      ..., # TODO
+      eri = -123
+    ), 
+    "'eri' must be positive"
+  )
+
+  expect_error(
+    pbd_sim_checked(
+      ..., # TODO
+      scr = -123
+    ), 
+    "'scr' must be positive"
+  )
+  
+  expect_error(
+    pbd_sim_checked(
+      ..., # TODO
+      sirg = -123
+    ), 
+    "'sirg' must be non-zero and positive"
+  )
+
+  expect_error(
+    pbd_sim_checked(
+      ..., # TODO
+      siri = -123
+    ), 
+    "'siri' must be non-zero and positive"
+  )
+
+  expect_error(
+    pbd_sim_checked(
+      ..., # TODO
+      crown_age = NULL,
+      stem_age = NULL
+    ), 
+    "At least one of 'crown_age' or 'stem_age' must be non-zero and positive" 
+  )
+
+  expect_error(
+    pbd_sim_checked(
+      ..., # TODO
+      crown_age = -123,
+      stem_age = NULL
+    ), 
+    "'crown_age' must be non-zero and positive" 
+  )
+
+  expect_error(
+    pbd_sim_checked(
+      ..., # TODO
+      crown_age = NULL,
+      stem_age = -123
+    ), 
+    "'stem_age' must be non-zero and positive" 
+  )
+
+  expect_error(
+    pbd_sim_checked(
+      ..., # TODO
+      crown_age = 123,
+      stem_age = 234
+    ), 
+    "Must set either 'crown_age' or 'stem_age'" 
+  )
+
+})
