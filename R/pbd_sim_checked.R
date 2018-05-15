94662dbd9905596257450b928dc735c93ff8bcb6
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-15 07:48:44 +0200

    @lintr-bot

--- R/pbd_sim_checked.R
+++ R/pbd_sim_checked.R
@@ -6,7 +6,7 @@
 #' @param siri speciation initiation rate of an incipient species
 #' @param stem_age stem age. Set either the stem age or the crown age.
 #' @param crown_age crown age. Set either the stem age or the crown age.
-#' @param max_n_taxa maximum number of taxa. If this value is exceeded, 
+#' @param max_n_taxa maximum number of taxa. If this value is exceeded,
 #'   the simulation is aborted and removed.
 pbd_sim_checked <- function(
   erg,
@@ -16,7 +16,7 @@ pbd_sim_checked <- function(
   siri,
   stem_age = NULL,
   crown_age = NULL,
-  max_n_taxa = Inf  
+  max_n_taxa = Inf
 ) {
   # TODO: use if and stop
   testit::assert(erg >= 0.0)
@@ -26,13 +26,13 @@ pbd_sim_checked <- function(
   testit::assert(scr >= 0.0)
   testit::assert(xor(is.null(crown_age), is.null(stem_age)))
   testit::assert(max_n_taxa >= 0.0)
-  
+
   # age
   age <- stem_age
   if (is.null(age)) age <- crown_age
   testit::assert(!is.null(age))
   testit::assert(age > 0.0)
-  
+
   # soc
   soc <- 1
   if (!is.null(crown_age)) soc <- 2
--- tests/testthat/test-pbd_sim_checked.R
+++ tests/testthat/test-pbd_sim_checked.R
@@ -1,9 +1,9 @@
 context("pbd_sim_checked")
 
 test_that("use", {
-  
+
   skip("TODO for @J-Damhuis and @Tomdkkr")
-  
+
   expect_silent(
     pbd_sim_checked(
       ... # TODO
@@ -12,12 +12,12 @@ test_that("use", {
 })
 
 test_that("PBD::pbd_sim and pbd_sim_checked must give same results", {
-  
+
   skip("TODO for @J-Damhuis and @Tomdkkr")
-  
+
   # This will be harder!
   # You will have to duck to compare elements of lists
-  
+
   expect_equal(
     pbd_sim_checked(
       ... # TODO
@@ -31,12 +31,12 @@ test_that("PBD::pbd_sim and pbd_sim_checked must give same results", {
 test_that("abuse", {
 
   skip("TODO for @J-Damhuis and @Tomdkkr")
-  
+
   expect_error(
     pbd_sim_checked(
       ..., # TODO
       erg = -123
-    ), 
+    ),
     "'erg' must be positive"
   )
 
@@ -44,7 +44,7 @@ test_that("abuse", {
     pbd_sim_checked(
       ..., # TODO
       eri = -123
-    ), 
+    ),
     "'eri' must be positive"
   )
 
@@ -52,15 +52,15 @@ test_that("abuse", {
     pbd_sim_checked(
       ..., # TODO
       scr = -123
-    ), 
+    ),
     "'scr' must be positive"
   )
-  
+
   expect_error(
     pbd_sim_checked(
       ..., # TODO
       sirg = -123
-    ), 
+    ),
     "'sirg' must be non-zero and positive"
   )
 
@@ -68,7 +68,7 @@ test_that("abuse", {
     pbd_sim_checked(
       ..., # TODO
       siri = -123
-    ), 
+    ),
     "'siri' must be non-zero and positive"
   )
 
@@ -77,8 +77,8 @@ test_that("abuse", {
       ..., # TODO
       crown_age = NULL,
       stem_age = NULL
-    ), 
-    "At least one of 'crown_age' or 'stem_age' must be non-zero and positive" 
+    ),
+    "At least one of 'crown_age' or 'stem_age' must be non-zero and positive"
   )
 
   expect_error(
@@ -86,8 +86,8 @@ test_that("abuse", {
       ..., # TODO
       crown_age = -123,
       stem_age = NULL
-    ), 
-    "'crown_age' must be non-zero and positive" 
+    ),
+    "'crown_age' must be non-zero and positive"
   )
 
   expect_error(
@@ -95,8 +95,8 @@ test_that("abuse", {
       ..., # TODO
       crown_age = NULL,
       stem_age = -123
-    ), 
-    "'stem_age' must be non-zero and positive" 
+    ),
+    "'stem_age' must be non-zero and positive"
   )
 
   expect_error(
@@ -104,8 +104,8 @@ test_that("abuse", {
       ..., # TODO
       crown_age = 123,
       stem_age = 234
-    ), 
-    "Must set either 'crown_age' or 'stem_age'" 
+    ),
+    "Must set either 'crown_age' or 'stem_age'"
   )
 
 })
