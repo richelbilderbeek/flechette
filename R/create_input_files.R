701cda57428576575e38714a86f5d2016e49b2e4
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-22 11:27:29 +0200

    Add 'create_input_files_sampling'

--- R/create_input_files.R
+++ R/create_input_files.R
@@ -9,13 +9,47 @@ create_input_files_general <- function(
   general_params_set = create_general_params_set(),
   folder_name = getwd()
 ) {
+  create_input_files_impl(
+    params_set = general_params_set,
+    folder_name = folder_name
+  )
+}
+
+#' Creates the parameter files in the article for the general exploration
+#' @param sampling_params_set the set of parameters,
+#'   as created by \code{\link{create_sampling_params_set}}
+#' @param folder_name name of the folder where all files are created
+#' @return The filenames of all parameter files created
+#' @export
+#' @author Richel Bilderbeek
+create_input_files_sampling <- function(
+  sampling_params_set = create_sampling_params_set(),
+  folder_name = getwd()
+) {
+  create_input_files_impl(
+    params_set = sampling_params_set,
+    folder_name = folder_name
+  )
+}
+
+#' Creates the parameter files in the article for the general exploration
+#' @param params_set the set of parameters,
+#'   as created by \code{\link{create_general_params_set}}
+#'   or \code{\link{create_sampling_params_set}}  
+#' @param folder_name name of the folder where all files are created
+#' @return The filenames of all parameter files created
+#' @author Richel Bilderbeek
+create_input_files_impl <- function(
+  params_set,
+  folder_name
+) {
   filenames <- NULL
   # Must start at one, as the BEAST2 RNG seed must be at least one.
   index <- 1
-  n_files <- length(general_params_set)
+  n_files <- length(params_set)
   for (index in seq(1, n_files)) {
     filename <- file.path(folder_name, paste0(index, ".RDa"))
-    params <- general_params_set[[index]]
+    params <- params_set[[index]]
     saveRDS(object = params, file = filename)
     index <- index + 1
     filenames <- c(filenames, filename)
