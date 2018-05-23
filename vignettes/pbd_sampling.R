bc3a8cc008a5b7033f90add9ba7fbf38b813dd76
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-23 09:35:07 +0200

    Rebuild vignette

--- vignettes/pbd_sampling.R
+++ vignettes/pbd_sampling.R
@@ -21,15 +21,13 @@ plot <- function(out) {
   testit::assert(class(out$stree_oldest) == "phylo")
   testit::assert(class(out$stree_random) == "phylo")
 
-  graphics::par(mfrow = c(1, 6), mar = c(5, 4, 6, 2) + 0.3)
+  graphics::par(mfrow = c(1, 4), mar = c(5, 4, 6, 2) + 0.3)
   cols <- stats::setNames(c("gray", "black"), c("i", "g"))
   phytools::plotSimmap(out$igtree.extant, colors = cols, fsize = 2)
 
   sum_youngest <- sum(out$stree_youngest$edge.length)
   sum_oldest <- sum(out$stree_oldest$edge.length)
   sum_random <- sum(out$stree_random$edge.length)
-  sum_shortest <- sum(out$stree_shortest$edge.length)
-  sum_longest <- sum(out$stree_longest$edge.length)
 
   ape::plot.phylo(out$stree_youngest, edge.width = 2, font = 1,
     label.offset = 0.1, cex = 2, cex.main = 0.75,
@@ -43,78 +41,58 @@ plot <- function(out) {
     label.offset = 0.1, cex = 2, cex.main = 0.75,
     main = paste("\n\n", "oldest", format(round(sum_oldest, 2), nsmall = 2), "\n\n"))
   ape::add.scale.bar()
-  ape::plot.phylo(out$stree_shortest, edge.width = 2, font = 1,
-    label.offset = 0.1, cex = 2, cex.main = 0.75,
-    main = paste("\n\n", "shortest", format(round(sum_shortest, 2), nsmall = 2), "\n\n"))
-  ape::add.scale.bar()
-  ape::plot.phylo(out$stree_longest, edge.width = 2, font = 1,
-    label.offset = 0.1, cex = 2, cex.main = 0.75,
-    main = paste("\n\n", "longest", format(round(sum_longest, 2), nsmall = 2), "\n\n"))
-  ape::add.scale.bar()
   graphics::par(mfrow = c(1, 1))
 }
 
 ## ----find_expected_simplest, fig.width=7, fig.height=5-------------------
-if (1 == 2) { # TODO: @J-Damhuis
-  out <- raket::pbd_find_scenario(
-    scr = 0.1, sirg = 2, siri = 2, scenario = "expected", 
-    rng_seed = 42, max_n_subspecies = 3
-  )
-  plot(out)
-}
+out <- raket::pbd_find_scenario(
+  scr = 0.1, sirg = 2, siri = 2, scenario = "expected", 
+  rng_seed = 42, max_n_subspecies = 3
+)
+plot(out)
 
 ## ----find_expected_simpler, fig.width=7, fig.height=5--------------------
-if (1 == 2) { # TODO: @J-Damhuis
-  out <- raket::pbd_find_scenario(
-    scr = 0.1, sirg = 2, siri = 2, scenario = "expected", 
-    rng_seed = 44, 
-    min_n_subspecies = 4,
-    max_n_subspecies = 4
-  )
-  plot(out)
-}
+out <- raket::pbd_find_scenario(
+  scr = 0.1, sirg = 2, siri = 2, scenario = "expected", 
+  rng_seed = 44, 
+  min_n_subspecies = 4,
+  max_n_subspecies = 4
+)
+plot(out)
 
 ## ----find_expected_simple, fig.width=7, fig.height=5---------------------
-if (1 == 2) { # TODO: @J-Damhuis
-  out <- raket::pbd_find_scenario(scr = 0.5, sirg = 1, siri = 2, scenario = "expected",
-    rng_seed = 51, 
-    min_n_subspecies = 5, 
-    max_n_subspecies = 10,
-    min_n_species = 3
-  )
-  plot(out)
-}
+out <- raket::pbd_find_scenario(scr = 0.5, sirg = 1, siri = 2, scenario = "expected",
+  rng_seed = 51, 
+  min_n_subspecies = 5, 
+  max_n_subspecies = 10,
+  min_n_species = 3
+)
+plot(out)
 
 ## ----fig.width=7, fig.height=5-------------------------------------------
-if (1 == 2) { # TODO: @J-Damhuis
-  out <- raket::pbd_find_scenario(scr = 0.5, sirg = 1, siri = 2, scenario = "ylto",
-    rng_seed = 54, 
-    min_n_subspecies = 4, 
-    max_n_subspecies = 4,
-    min_n_species = 3
-  )
-  plot(out)
-}
+out <- raket::pbd_find_scenario(scr = 0.5, sirg = 1, siri = 2, scenario = "ylto",
+  rng_seed = 54, 
+  min_n_subspecies = 4, 
+  max_n_subspecies = 4,
+  min_n_species = 3
+)
+plot(out)
 
 ## ----fig.width=7, fig.height=5-------------------------------------------
-if (1 == 2) { # TODO: @J-Damhuis
-  out <- raket::pbd_find_scenario(scr = 0.5, sirg = 1, siri = 2, scenario = "rsty",
-    rng_seed = 55, 
-    min_n_subspecies = 4, 
-    max_n_subspecies = 7,
-    min_n_species = 3
-  )
-  plot(out)
-}
+out <- raket::pbd_find_scenario(scr = 0.5, sirg = 1, siri = 2, scenario = "rsty",
+  rng_seed = 55, 
+  min_n_subspecies = 4, 
+  max_n_subspecies = 7,
+  min_n_species = 3
+)
+plot(out)
 
 ## ----fig.width=7, fig.height=5-------------------------------------------
-if (1 == 2) { # TODO: @J-Damhuis
-  out <- raket::pbd_find_scenario(scr = 0.5, sirg = 1, siri = 2, scenario = "rlto",
-    rng_seed = 57, 
-    min_n_subspecies = 4, 
-    max_n_subspecies = 7,
-    min_n_species = 3
-  )
-  plot(out)
-}
+out <- raket::pbd_find_scenario(scr = 0.5, sirg = 1, siri = 2, scenario = "rlto",
+  rng_seed = 57, 
+  min_n_subspecies = 4, 
+  max_n_subspecies = 7,
+  min_n_species = 3
+)
+plot(out)
 
