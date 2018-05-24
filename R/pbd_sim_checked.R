433c323775b401d05b6b361e8c0a726fbde9d38b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-24 14:35:27 +0200

    @lintr-bot

--- R/pbd_sim_checked.R
+++ R/pbd_sim_checked.R
@@ -9,7 +9,7 @@
 #' @param max_n_taxa maximum number of taxa. If this value is exceeded,
 #'   the simulation is aborted and removed.
 #' @param add_shortest_and_longest Gives the output of the new samplemethods
-#'   'shortest' and 'longest'. 
+#'   'shortest' and 'longest'.
 #' @export
 pbd_sim_checked <- function(
   erg,
