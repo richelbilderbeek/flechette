54d035ea6cdab6ac16cd1954e38046fb2e01681a
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-28 09:55:27 +0200

    'create_input_files' is tested again

--- tests/testthat/test-create_input_files.R
+++ tests/testthat/test-create_input_files.R
@@ -2,7 +2,6 @@ context("create_input_files")
 
 test_that("files are created in right folder", {
 
-  skip("Skip all tests")
   filenames <- create_input_files_general(
     general_params_set = create_general_params_set(),
     folder_name = tempdir()
