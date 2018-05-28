facade94161a39e47966e42865a40a302ac386e2
richelbilderbeek <richel@richelbilderbeek.nl>
2018-05-28 12:01:29 +0200

    Move 'pbd_sim_checked' to PBD package

--- R/pbd_sim_checked.R
+++ R/pbd_sim_checked.R
@@ -22,59 +22,15 @@ pbd_sim_checked <- function(
   max_n_taxa = Inf,
   add_shortest_and_longest = FALSE
 ) {
-  if (erg < 0.0) {
-    stop("'erg' must be positive")
-  }
-  if (eri < 0.0) {
-    stop("'eri' must be positive")
-  }
-  if (sirg < 0.0) {
-    stop("'sirg' must be positive")
-  }
-  if (siri < 0.0) {
-    stop("'siri' must be positive")
-  }
-  if (scr < 0.0) {
-    stop("'scr' must be positive")
-  }
-  if (is.null(crown_age)) {
-    if (is.null(stem_age)) {
-      stop(
-        "At least one of 'crown_age' or 'stem_age' ",
-        "must be non-zero and positive"
-      )
-    } else if (stem_age <= 0.0) {
-      stop("'stem_age' must be non-zero and positive")
-    }
-  } else if (crown_age <= 0.0) {
-    stop("'crown_age' must be non-zero and positive")
-  } else if (crown_age > 0.0) {
-    if (!is.null(stem_age)) {
-      stop("Must set either 'crown_age' or 'stem_age'")
-    }
-  }
-  if (max_n_taxa < 0.0) {
-    stop("'max_n_taxa' must be positive")
-  }
-
-  # age
-  age <- stem_age
-  if (is.null(age)) age <- crown_age
-  testit::assert(!is.null(age))
-  testit::assert(age > 0.0)
-
-  # soc
-  soc <- 1
-  if (!is.null(crown_age)) soc <- 2
-  testit::assert(!is.null(soc))
-  testit::assert(soc == 1 || soc == 2)
-
-  PBD::pbd_sim(
-    pars = c(sirg, scr, siri, erg, eri),
-    age = age,
-    soc = soc,
-    plotit = FALSE,
-    limitsize = max_n_taxa,
+  PBD::pbd_sim_checked(
+    erg = erg,
+    eri = eri,
+    scr = scr,
+    sirg = sirg,
+    siri = siri,
+    stem_age = stem_age,
+    crown_age = crown_age,
+    max_n_taxa = max_n_taxa,
     add_shortest_and_longest = add_shortest_and_longest
   )
 }
