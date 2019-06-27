de21ff9a6c3287c7f73a2d31f0eb76390c2421fc
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-27 12:51:29 +0200

    Creating a sampling params set works

--- tests/testthat/test-create_sampling_params_set.R
+++ tests/testthat/test-create_sampling_params_set.R
@@ -14,30 +14,33 @@ test_that("must be a collection of multiple parameters", {
 
 test_that("no high SRCs", {
 
-  skip("Not now")
+  if (!beastier::is_on_travis()) return()
 
-  for (params in create_sampling_params_set(max_n_params = 2)) {
-    expect_true(params$scr < 1000.0)
+  params_set <- create_sampling_params_set(
+    max_n_params = 2
+  )
+  for (params in params_set) {
+    expect_true(params$pbd_params$scr < 1000.0)
   }
 })
 
 test_that("sampling matters", {
 
-  skip("Not now")
+  if (!beastier::is_on_travis()) return()
 
   params_set <- create_sampling_params_set(max_n_params = 2)
   for (params in params_set) {
     set.seed(params$tree_sim_rng_seed)
     out <- becosys::pbd_sim_checked(
-      erg = params$erg,
-      eri = params$eri,
-      scr = params$scr,
-      sirg = params$sirg,
-      siri = params$siri,
-      crown_age = params$inference_params$mrca_prior$mrca_distr$mean$value
+      erg = params$pbd_params$erg,
+      eri = params$pbd_params$eri,
+      scr = params$pbd_params$scr,
+      sirg = params$pbd_params$sirg,
+      siri = params$pbd_params$siri,
+      crown_age = params$pir_params$experiments[[1]]$inference_model$mrca_prior$mrca_distr$mean$value
     )
     sum_youngest <- sum(out$stree_youngest$edge.length)
     sum_oldest <- sum(out$stree_oldest$edge.length)
-    testthat::expect_true(sum_youngest != sum_oldest)
+    expect_true(sum_youngest != sum_oldest)
   }
 })
