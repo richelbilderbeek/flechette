99f814480512444b74bd5c2c5de703f4850d4300
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-17 11:46:12 +0200

    Pass Travis checks

--- tests/testthat/test-rkt_run_from_file.R
+++ tests/testthat/test-rkt_run_from_file.R
@@ -5,7 +5,7 @@ test_that("use", {
   if (!beastier::is_on_travis()) return()
 
   super_folder_name <- peregrine::get_pff_tempdir()
-  project_folder_name <- file.path(super_folder_name, "raket_project")
+  project_folder_name <- file.path(super_folder_name, "raket_werper")
   dir.create(path = project_folder_name, recursive = TRUE, showWarnings = FALSE)
   parameters_filenames <- create_files_raket_paramses(
     general_params_set = create_general_params_set(
