a4098ff0df49cc9a156bc0836b9f15c170c5c522
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-14 15:25:01 +0200

    Expose #36

--- tests/testthat/test-rkt_run.R
+++ tests/testthat/test-rkt_run.R
@@ -1,12 +1,9 @@
-context("rkt_run")
-
-test_that("use, random", {
-
-  skip("To be done")
+test_that("use, shortest", {
 
   if (!beastier::is_on_travis()) return()
 
   raket_params <- create_test_raket_params()
+  testit::assert(raket_params$sampling_method == "shortest")
   out <- run_raket(raket_params)
   expect_true("parameters" %in% names(out))
   expect_true("incipient_tree" %in% names(out))
@@ -18,20 +15,12 @@ test_that("use, random", {
 
 test_that("use, random", {
 
-  skip("To be done")
-
   if (!beastier::is_on_travis()) return()
 
+  skip("Sum of evidences deviates too much from one")
   raket_params <- create_test_raket_params()
-
-  for (sampling_method in rkt_get_sampling_methods()) {
-    raket_params$sampling_method <- sampling_method
-    out <- rkt_run(raket_params = raket_params)
-    expect_true("parameters" %in% names(out))
-    expect_true("incipient_tree" %in% names(out))
-    expect_true("species_tree" %in% names(out))
-    expect_true("alignment" %in% names(out))
-    expect_true("trees" %in% names(out))
-    expect_true("estimates" %in% names(out))
-  }
+  raket_params$sampling_method <- "random"
+  expect_silent(run_raket(raket_params))
 })
+
+
