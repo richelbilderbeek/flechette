1c3c4f25f86e106a9eaf4f51b565e9484d1e1a01
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-26 18:38:32 +0200

    EOD

--- tests/testthat/test-create_test_params_set.R
+++ tests/testthat/test-create_test_params_set.R
@@ -1,3 +1,7 @@
 test_that("use", {
-  expect_silent(check_raket_paramses(create_test_params_set()))
+  expect_silent(
+    check_raket_paramses(
+      create_test_params_set()
+    )
+  )
 })
--- tests/testthat/test-create_test_raket_params.R
+++ tests/testthat/test-create_test_raket_params.R
@@ -1,5 +1,9 @@
 context("test-create_test_raket_params")
 
 test_that("use", {
-  expect_silent(create_test_raket_params())
+  expect_silent(
+    check_raket_params(
+      create_test_raket_params()
+    )
+  )
 })
