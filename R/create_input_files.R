1dd80eb4d9a65973c792c7a7fa8dc39a4eda9c74
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-28 13:06:18 +0200

    Renamed 'create_input_files_impl' to 'create_input_files' as that is what it does :-)

--- R/create_input_files.R
+++ R/create_input_files.R
@@ -1,55 +1,16 @@
-#' Creates the parameter files in the article for the general exploration
-#' @param general_params_set the set of parameters,
-#'   as created by \code{\link{create_general_params_set}}
-#' @param project_folder_name name of the project folder.
-#'   The name of this folder must be \code{raket_werper}.
-#' @return The filenames of all parameter files created
-#' @export create_input_files_general create_files_raket_paramses
-#' @aliases create_input_files_general create_files_raket_paramses
-#' @author Richel Bilderbeek
-create_input_files_general <- create_files_raket_paramses <- function(
-  raket_paramses = create_general_params_set()
-) {
-  create_input_files_impl( # nolint internal function
-    raket_paramses = raket_paramses
-  )
-}
-
-#' Creates the parameter files in the article for the general exploration
-#' @param sampling_params_set the set of parameters,
-#'   as created by \code{\link{create_sampling_params_set}}
-#' @param folder_name name of the folder where all files are created
-#' @return The filenames of all parameter files created
-#' @export
-#' @author Richel Bilderbeek
-create_input_files_sampling <- function(
-  sampling_params_set = create_sampling_params_set(),
-  folder_name = getwd()
-) {
-  create_input_files_impl(
-    raket_paramses = sampling_params_set,
-    folder_name = folder_name
-  )
-}
-
-#' Creates the parameter files in the article for the general exploration
-#' @param sampling_params_set the set of parameters,
-#'   as created by \code{\link{create_sampling_params_set}}
-#' @param folder_name name of the folder where all files are created
-#' @return The filenames of all parameter files created
-#' @export
-#' @author Richel Bilderbeek
-create_input_files_test <- function(
-  raket_paramses = create_test_params_set()
-) {
-  create_input_files_impl(raket_paramses) #  nolint raket function
-}
-#' Creates the parameter files in the article for the general exploration
+#' Creates the parameter files from the \code{raket_params}
 #' @inheritParams default_params_doc
 #' @param folder_name name of the folder where all files are created
 #' @return The filenames of all parameter files created
+#' @seealso
+#' Use \link{create_input_files_test} to create the parameters file
+#' for a test input set.
+#' Use \link{create_input_files_general} to create the parameters file
+#' for a general input set.
+#' Use \link{create_input_files_sampling} to create the parameters file
+#' for a sampling input set.
 #' @author Richel Bilderbeek
-create_input_files_impl <- function(
+create_input_files <- function(
   raket_paramses
 ) {
   check_raket_paramses(raket_paramses) # nolint raket function
@@ -65,4 +26,47 @@ create_input_files_impl <- function(
     index <- index + 1
   }
   filenames
+}
+
+#' Creates the parameter files in the article for the general exploration
+#' @param general_params_set the set of parameters,
+#'   as created by \code{\link{create_general_params_set}}
+#' @param project_folder_name name of the project folder.
+#'   The name of this folder must be \code{raket_werper}.
+#' @return The filenames of all parameter files created
+#' @export create_input_files_general create_files_raket_paramses
+#' @aliases create_input_files_general create_files_raket_paramses
+#' @author Richel Bilderbeek
+create_input_files_general <- create_files_raket_paramses <- function(
+  raket_paramses = create_general_params_set()
+) {
+  create_input_files(raket_paramses) # nolint internal function
+}
+
+#' Creates the parameter files in the article for the general exploration
+#' @param sampling_params_set the set of parameters,
+#'   as created by \code{\link{create_sampling_params_set}}
+#' @param folder_name name of the folder where all files are created
+#' @return The filenames of all parameter files created
+#' @export
+#' @author Richel Bilderbeek
+create_input_files_sampling <- function(
+  raket_paramses = create_sampling_params_set(
+    project_folder_name = getwd()
+  )
+) {
+  create_input_files(raket_paramses) # nolint internal function
+}
+
+#' Creates the parameter files in the article for the general exploration
+#' @param sampling_params_set the set of parameters,
+#'   as created by \code{\link{create_sampling_params_set}}
+#' @param folder_name name of the folder where all files are created
+#' @return The filenames of all parameter files created
+#' @export
+#' @author Richel Bilderbeek
+create_input_files_test <- function(
+  raket_paramses = create_test_params_set(project_folder_name = getwd())
+) {
+  create_input_files(raket_paramses) # nolint internal function
 }
