1f0279c1fa14459aa38670f8a71177593ba47632
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-28 15:36:41 +0200

    Added 'create_pbd_params_file'

---
+++ R/create_pbd_params_file.R
@@ -0,0 +1,18 @@
+#' Collect all PBD parameters of a \code{raket_werper} run and put these
+#' in one \code{.csv} file called \code{results/pbd_params.csv}
+#' @inheritParams default_params_doc
+#' @return the name of the file created
+#' @author Richel J.C. Bilderbeek
+#' @export
+create_pbd_params_file <- function(
+  project_folder_name = get_raket_path("raket_werper")
+) {
+  df <- collect_pbd_params(project_folder_name)
+  filename <- file.path(project_folder_name, "results", "pbd_params.csv")
+  dir.create(path = dirname(filename), showWarnings = FALSE, recursive = TRUE)
+  write.csv(
+    x = df,
+    file = filename
+  )
+  filename
+}
